0be0a47bceda69f74ac3d3d25653e12f9d4aaf83
Amber Kumar <amb3rkum4r@gmail.com>
2025-06-16 05:32:57 +0530

    bhjbh

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { ModalProvider } from './context/ModalContext';
+import './index.css';  // ✅ Import your global CSS here (Tailwind usually)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
