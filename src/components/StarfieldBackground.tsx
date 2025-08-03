d84238e5e2adf46ad389ca6cfc7c885d880c14ac
Amber Kumar <amb3rkum4r@gmail.com>
2025-08-04 00:59:30 +0530

    8989

--- src/components/StarfieldBackground.tsx
+++ src/components/StarfieldBackground.tsx
@@ -3,13 +3,11 @@ import { FC, useRef } from 'react';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { Stars } from '@react-three/drei';
 import * as THREE from 'three';
+import { EffectComposer, Bloom, ChromaticAberration } from '@react-three/postprocessing';
 
 // Define the colors from your CSS for a cohesive look
 const primaryColor = new THREE.Color("hsl(260, 80%, 65%)");
 const secondaryColor = new THREE.Color("hsl(200, 100%, 55%)");
-const cyanColor = new THREE.Color("hsl(180, 100%, 50%)");
-const purpleColor = new THREE.Color("hsl(280, 65%, 60%)");
-
 
 const DynamicStarfield = () => {
     const { camera } = useThree();
@@ -18,7 +16,6 @@ const DynamicStarfield = () => {
     const starsRef2 = useRef<any>(null);
     const starsRef3 = useRef<any>(null);
     const starsRef4 = useRef<any>(null);
-
 
     useFrame(({ clock }) => {
         // Animate camera position for a subtle drift
@@ -47,8 +44,8 @@ const DynamicStarfield = () => {
             <pointLight ref={lightRef} position={[0, 0, 0]} intensity={1.5} distance={100} />
             <Stars ref={starsRef1} radius={100} depth={50} count={5000} factor={4} saturation={0.8} fade speed={1} />
             <Stars ref={starsRef2} radius={80} depth={30} count={1500} factor={6} saturation={0.6} fade speed={2} />
-            <Stars ref={starsRef3} radius={60} depth={20} count={1000} factor={5} saturation={1} fade speed={3} material-color={cyanColor} />
-            <Stars ref={starsRef4} radius={120} depth={60} count={2000} factor={3} saturation={1} fade speed={0.5} material-color={purpleColor} />
+            <Stars ref={starsRef3} radius={60} depth={20} count={1000} factor={5} saturation={1} fade speed={3} />
+            <Stars ref={starsRef4} radius={120} depth={60} count={2000} factor={3} saturation={1} fade speed={0.5}  />
         </>
     );
 };
@@ -58,8 +55,12 @@ const StarfieldBackground: FC = () => {
         <div className="fixed inset-0 z-0">
             <Canvas camera={{ position: [0, 0, 1] }}>
                 <color attach="background" args={['black']} />
-                <ambientLight intensity={0.2} />
-                <DynamicStarfield />
+                <ambientLight intensity={0.9} />
+                <EffectComposer multisampling={0}>
+                    <DynamicStarfield />
+                    <Bloom intensity={2} luminanceThreshold={0.5} luminanceSmoothing={0.2} /> 
+                    <ChromaticAberration radialModulation={true} modulationOffset={1.5}  />
+                </EffectComposer>
             </Canvas>
         </div>
     );
