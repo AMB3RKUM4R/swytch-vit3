cdaec9b2e4927fd0cef1fb8002d57b1614729ffc
Amber Kumar <amb3rkum4r@gmail.com>
2025-12-08 19:38:36 +0530

    opop

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -9,7 +9,7 @@ const Shop: FC = () => {
   const { setActiveModal, setShowMessage } = useModal();
   const { userId } = usePlayer();
 
-  const handlePurchase = (id?: string) => {
+  const handlePurchase = () => {
     if(!userId) {
         setShowMessage("⚠️ AUTH REQUIRED");
         setActiveModal('auth');
